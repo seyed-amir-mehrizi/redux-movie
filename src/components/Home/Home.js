93564a00c98d2536a6991172942af85cc706a3dc
Seyed Amir Mehrizi <s.a.mehrizi1989@gmail.com>
2021-12-26 22:36:49 +0330

    add store to the project and write the first reducer and dispatch method

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,16 +3,19 @@ import React, { useEffect } from 'react'
 import MovieListing from '../MovieListing/MovieListing'
 import movieApi from '../../common/api/movieApi';
 import { apiKey } from '../../common/api/movieApiKey';
+import { useDispatch } from 'react-redux';
+import { addMovies } from '../../features/movies/movieSlice';
 export default function Home() {
 
+    const dispatch  = useDispatch();
     useEffect(() => {
-        const movieText = "Harry"
+        const movieText = "Harry";
         const fetchMovies = async () => {
             const response = await axios.get(`http://www.omdbapi.com/?i=tt3896198&apiKey=${apiKey}&s=${movieText}&type=movie`)
                 .catch((err) => {
                     console.log(err)
                 })
-                console.log(response);
+                dispatch(addMovies(response.data))
         }
         fetchMovies();
     }, []);
